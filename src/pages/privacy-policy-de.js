64dcc1a5a5d39fde4993a21c6c2e95ec3b740be9
Sandro Scalco <sandro.scalco@liitu.ch>
2023-11-20 09:31:15 +0100

    feat update text

---
+++ src/pages/privacy-policy-de.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { Main } from '@layout';
+import { Seo } from '@common';
+import { StyledAbout } from '@about/styles';
+
+export default function PrivacyPolicyPage() {
+  return (
+    <Main>
+      <Seo title="PrivacyPolicy" description="PrivacyPolicy" />
+      <StyledAbout>
+        <h1>Privacy Policy</h1>
+        <div>
+          <div className="section starter">
+            <h2>Introduction</h2>
+            <p>Welcome to myclub | the next generation. This Privacy Policy outlines our practices regarding the collection, use, and disclosure of your information when you use our app and the choices you have associated with that information.</p>
+            <p></p>
+          </div>
+          <div className="section starter">
+            <h2>Information Collection and Use</h2>
+            <p>For a better experience while using our app, we may require you to provide us with certain personally identifiable information, including but not limited to your email address, first and last name. The information that we collect will be used to identify and manage your club membership and is necessary for the functionality of our app.</p>
+            <p>a. Profile Creation</p>
+            <p><ul>
+              <li>Required Information: To create an account, we need your email address, first and last name. This is essential for club administrators to approve your membership request.</li>
+              <li>Additional Information: Your home address and phone number are collected for the administration of your club membership and are visible only to your club and team administrators.</li>
+              </ul>
+              </p>
+              <p>b. Data Hosting</p>
+            <p><ul>
+              <li>Your data is processed and stored in Switzerland (Europe-West6 region), ensuring compliance with local data protection and privacy regulations.</li>
+              </ul>
+              </p>              
+          </div>
+
+          <div className="section starter">
+            <h2>Deleting Your Profile</h2>
+            <p>You have the option to delete your profile directly within the app. Navigate to your profile page, scroll to the bottom, and select "Danger Zone: Profil löschen". Please note that this action is immediate, irreversible, and will result in the deletion of all your personal data from our systems.</p>
+          </div>
+          <div className="section starter">
+            <h2> Location Data</h2>
+            <p>Our app does not store your location data. Location services are used solely for providing navigation assistance for your games and events.</p>
+          </div>
+          <div className="section starter">
+            <h2>Security</h2>
+            <p>We value your trust in providing us your Personal Information, and we strive to use commercially acceptable means of protecting it. However, remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+          </div>
+          <div className="section starter">
+            <h2>Changes to This Privacy Policy</h2>
+            <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.</p>
+          </div>
+          <div className="section starter">
+            <h2>Contact Us</h2>
+            <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at info@my-club.app.</p>
+          </div>
+        </div>
+      </StyledAbout>
+    </Main>
+  );
+}
---
+++ src/pages/terms-and-conditions-de.js
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import { Main } from '@layout';
+import { Seo } from '@common';
+import { StyledAbout } from '@about/styles';
+
+export default function TermsAndConditionsPage() {
+  return (
+    <Main>
+      <Seo title="TermsAndConditionsDE" description="TermsAndConditionsDE" />
+      <StyledAbout>
+        <h1>Geschäftsbedingungen</h1>
+        <div>
+          <div className="section starter">
+            <h2>Einführung</h2>
+            <p>Willkommen bei myclub | the next generation. Diese Nutzungsbedingungen ("Bedingungen") regeln Ihre Nutzung der myclub-Website und mobilen Anwendung ("Dienst"). Durch den Zugriff auf oder die Nutzung unseres Dienstes erklären Sie sich mit diesen Bedingungen und unserer Datenschutzrichtlinie einverstanden.</p>
+          </div>
+          <div className="section starter">
+            <h2>Nutzung des Dienstes</h2>
+            <p>a. Der Dienst ist für Benutzer gedacht, die mindestens 13 Jahre alt sind.</p>
+            <p>b. Sie dürfen den Dienst nur zu rechtmäßigen Zwecken und in Übereinstimmung mit diesen Bedingungen nutzen.</p>
+            <p>c. Jede Nutzung oder Zugriff durch Personen unter 13 Jahren ist verboten.</p>
+          </div>
+          <div className="section starter">
+            <h2>Konten</h2>
+            <p>a. Um bestimmte Funktionen des Dienstes nutzen zu können, müssen Sie ein Konto erstellen.</p>
+            <p>b. Sie sind verantwortlich für die Wahrung der Vertraulichkeit Ihrer Kontozugangsdaten.</p>
+            <p>c. Sie erklären sich damit einverstanden, uns sofort zu informieren, wenn Sie von einer Verletzung der Sicherheit oder einer unbefugten Nutzung Ihres Kontos Kenntnis erlangen.</p>
+          </div>
+          <div className="section starter">
+            <h2>Urheberrechte</h2>
+            <p>a. Der Dienst und seine ursprünglichen Inhalte, Merkmale und Funktionalitäten sind und bleiben das ausschließliche Eigentum von myclub | the next generation und seinen Lizenzgebern.</p>
+            <p>b. Unsere Warenzeichen und Geschäftsausstattungen dürfen nicht in Verbindung mit einem Produkt oder Dienst ohne unsere vorherige schriftliche Zustimmung verwendet werden.</p>
+          </div>
+          <div className="section starter">
+            <h2>Nutzerinhalte</h2>
+            <p>a. Sie behalten alle Rechte an den Inhalten, die Sie an den Dienst übermitteln, und sind allein verantwortlich dafür.</p>
+            <p>b. Durch das Veröffentlichen von Inhalten gewähren Sie myclub eine weltweite, nicht-exklusive, gebührenfreie Lizenz zur Nutzung, Vervielfältigung und Darstellung dieser Inhalte in Verbindung mit dem Dienst.</p>
+          </div>
+          <div className="section starter">
+            <h2>Verbotene Aktivitäten</h2>
+            <p>Sie erklären sich damit einverstanden, keine der folgenden verbotenen Aktivitäten auszuüben:
+              <ul>
+                <li>Illegale Aktivitäten: Nutzung des Dienstes zu illegalen Zwecken oder Verstoß gegen lokale, staatliche, nationale oder internationale Gesetze oder Vorschriften.</li>
+                <li>Verletzung von Urheberrechten: Kopieren, Verteilen oder Offenlegen eines Teils des Dienstes in irgendeinem Medium, einschließlich, aber nicht beschränkt auf automatisiertes oder nicht automatisiertes „Scraping“, oder Reproduzieren oder Umgehen der Navigationsstruktur oder Darstellung des Dienstes ohne unsere vorherige schriftliche Zustimmung.</li>
+                <li>Belästigung und Missbrauch: Belästigung, Mobbing oder diskriminierendes Verhalten gegenüber anderen Benutzern; Veröffentlichung oder Anzeige von Inhalten, die beleidigend, verleumderisch, diffamierend oder hasserfüllt sind.</li>
+                <li>Falsche Informationen: Angabe falscher Informationen bei Ihrer Registrierung oder Ihrem Profil oder Vortäuschen einer anderen Person.</li>
+                <li>Hacking und unbefugter Zugriff: Versuch, die Systemintegrität oder -sicherheit zu beeinträchtigen oder zu kompromittieren oder Übertragungen an oder von den Servern, die den Dienst betreiben, zu entschlüsseln.</li>
+                <li>Malware und Viren: Hochladen oder Übertragen von Viren, Malware oder anderer Arten von bösartigem Code, der in irgendeiner Weise verwendet wird oder werden könnte, um die Funktionalität oder den Betrieb des Dienstes zu beeinträchtigen.</li>
+                <li>Spamming: Automatisierte Nutzung des Systems, wie z.B. das Verwenden von Skripten zum Senden von Kommentaren oder Nachrichten, oder die Nutzung des Dienstes zum Senden unerwünschter Kommunikation.</li>
+                <li>Kommerzielle Aktivitäten: Nutzung des Dienstes zu kommerziellen Werbezwecken ohne unsere vorherige schriftliche Zustimmung.</li>
+                <li>Verletzungen des Inhalts: Veröffentlichung oder Hochladen von Inhalten, die die Rechte anderer verletzen, einschließlich Urheber-, Privatsphäre- oder Publicity-Rechte.</li>
+                <li>Störung des Dienstes: Beeinträchtigung, Störung oder Erzeugung einer unzumutbaren Belastung für den Dienst oder die mit dem Dienst verbundenen Netzwerke oder Dienste.</li>
+                <li>Verletzung der Privatsphäre: Versuch, persönliche Informationen anderer zu sammeln oder zu verfolgen; Eingriff in die Privatsphäre anderer, indem versucht wird, ohne Erlaubnis auf sensible Informationen zuzugreifen.</li>
+              </ul>
+            </p>
+          </div>
+          <div className="section starter">
+            <h2>Kündigung</h2>
+            <p>Wir können den Zugang zu unserem Dienst sofort und ohne vorherige Ankündigung oder Haftung aus irgendeinem Grund beenden oder aussetzen, einschließlich, aber nicht beschränkt auf, einen Verstoß gegen die Bedingungen.</p>
+          </div>
+          <div className="section starter">
+            <h2>Haftungsausschluss</h2>
+            <p>Der Dienst wird auf einer "AS IS" und "AS AVAILABLE" Basis bereitgestellt.</p>
+          </div>
+          <div className="section starter">
+            <h2>Haftungsbeschränkung</h2>
+            <p>Unter keinen Umständen haften myclub | the next generation oder seine Direktoren, Mitarbeiter für indirekte, zufällige, besondere, folgenschwere oder strafende Schäden, die aus Ihrer Nutzung des Dienstes entstehen.</p>
+          </div>
+          <div className="section starter">
+            <h2>Gerichtsstand</h2>
+            <p>Diese Bedingungen unterliegen den Gesetzen der Schweiz (Schaffhausen).</p>
+          </div>
+          <div className="section starter">
+            <h2>Änderungen der Bedingungen</h2>
+            <p>Wir behalten uns das Recht vor, diese Bedingungen jederzeit nach eigenem Ermessen zu ändern oder zu ersetzen.</p>
+          </div>
+          <div className="section starter">
+            <h2>Kontaktieren Sie uns</h2>
+            <p>Wenn Sie Fragen zu diesen Bedingungen haben, kontaktieren Sie uns bitte unter info@my-club.app</p>
+          </div>
+        </div>
+      </StyledAbout>
+    </Main>
+  );
+}
